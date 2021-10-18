e41bfc694e32f856ee87a62843876c2c7a3bcd6d
Bit Handshake <adam@perlina.hu>
2021-10-18 21:04:26 +0200

    x4.4.1 #8

--- src/clj/x/server_media/process_handler.clj
+++ src/clj/x/server_media/process_handler.clj
@@ -104,9 +104,11 @@
   ;
   ; @return (map)
   [request]
+  (println "Process query")
   (if (user/request->authenticated? request)
       (let [query       (sync/request->query request)
             environment (assoc ENVIRONMENT :request request)]
+           (println "request-authenticated")
            (sync/process-query! (param environment)
                                 (param query)))
       (http/error-wrap {:error-message :permission-denied :status 401})))
