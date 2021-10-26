98863fc00c504d601c9e9e952230b9bcc2f5410a
Paul Cristian <paul931224@gmail.com>
2021-10-26 19:39:20 +0200

    Whoops. value-path should be a vector.

--- src/cljs/extensions/clients/client_list.cljs
+++ src/cljs/extensions/clients/client_list.cljs
@@ -100,7 +100,7 @@
                          :element       #'client-item
                          :request-id    :clients/request-clients!
                          ;:sortable?     true
-                         :value-path    :clients }])
+                         :value-path    [:clients]}])
 
 (defn- view
   ; WARNING! NON-PUBLIC! DO NOT USE!
