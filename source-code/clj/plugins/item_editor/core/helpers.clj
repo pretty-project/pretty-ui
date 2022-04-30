b27d4cf8d10b625b72e4161f4d2f4215d1021429
Bit Handshake <adam@perlina.hu>
2022-04-30 20:54:23 +0200

    x4.7.0 #18

--- source-code/clj/plugins/item_editor/core/helpers.clj
+++ source-code/clj/plugins/item_editor/core/helpers.clj
@@ -11,10 +11,6 @@
 ;; ----------------------------------------------------------------------------
 
 ; mid.plugins.item-editor.core.helpers
-(def add-item-label           core.helpers/add-item-label)
-(def edit-item-label          core.helpers/edit-item-label)
-(def new-item-label           core.helpers/new-item-label)
-(def unnamed-item-label       core.helpers/unnamed-item-label)
 (def component-id             core.helpers/component-id)
 (def default-item-path        core.helpers/default-item-path)
 (def default-suggestions-path core.helpers/default-suggestions-path)
